edf20b1267b7f9a7a2d0366de9ce06f64b2c1d61
VietDinh17 <vietqudinh@gmail.com>
2018-05-03 21:37:48 -0700

    Implement Login Screen

--- App.js
+++ App.js
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Expo, { AppLoading, Asset, Font } from 'expo';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { StyleSheet, Text, View } from 'react-native';
 import { MapView } from 'expo';
-import Login from './src/screens/Login'
+import RootNavigation from './src/navigation/RootNavigation';
+import LoginScreen from './src/screens/Login';
 
 
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    isReady: false,
+  };
+
+  async _cacheResourcesAsync() {
+    const images = [
+      require('./assets/images/bg_screen1.jpg'),
+      require('./assets/images/bg_screen2.jpg'),
+      require('./assets/images/bg_screen3.jpg'),
+      require('./assets/images/bg_screen4.jpg'),
+      require('./assets/images/user-cool.png'),
+      require('./assets/images/user-hp.png'),
+      require('./assets/images/user-student.png'),
+      require('./assets/images/avatar1.jpg'),
+    ];
+    const cacheImages = images.map((image) => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+    
+    const fontAssets = cacheFonts([FontAwesome.font, Ionicons.font]);
+
+    await Promise.all(cacheImages,[...fontAssets]);
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
     return (
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+      <LoginScreen/>
     );
   }
 }
+
+Expo.registerRootComponent(App);
 
 const styles = StyleSheet.create({
   container: {
